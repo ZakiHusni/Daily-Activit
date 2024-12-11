267939aa7828d41a9ac1998728d6ce98af975837
alifnrz27 <alifnaufalrizki27@gmail.com>
2024-12-11 21:33:54 +0700

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="px-6 py-4 flex justify-end border-b border-black">
+    <div class="flex items-center">
+        <div class="mr-4">
+            Rizky Nugraha
+        </div>
+        <div class="mr-3">
+            <img class="w-[50px] h-[50px] rounded-full" src="https://img-cdn.medkomtek.com/IV1W_mcOx9M79-v2zMveBid70xE=/510x395/smart/filters:quality(100):format(webp)/article/SYWcOyU3eEzXzo_253fYl/original/1661919765-Mengapa%20Orang%20Mudah%20Marah.jpg" alt="">
+        </div>
+        <div>
+            <i class="fa-solid fa-caret-down"></i>
+        </div>
+    </div>
+</div>
